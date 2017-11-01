dac7bb7b1d501969c704ce2f69df9f0a4e5167cb
Andrew Clarke <andreweskeclarke@gmail.com>
2017-11-01 10:39:05 +0000

    Add git alias to setup script

--- setup.sh
+++ setup.sh
@@ -10,3 +10,9 @@ do
     fi
     ln -sf `pwd`/$DOTFILE $HOME/$DOTFILE
 done
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
